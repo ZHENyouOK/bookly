dc545387b2c4855894c36c89652a5fd2123438f1
Eugenia <zhenyouok@gmail.com>
2023-06-03 19:20:12 +0300

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,23 @@
+export const API = {
+  URL: "http://openlibrary.org/",
+  KEY: "53137aa142msh865fa4442983060p1199ecjsn836ad9b6b109",
+};
+
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyDUV7ikhYmEbhzDBp4vvonx43KJ4vDFa1o",
+//   authDomain: "weathy-app-43294.firebaseapp.com",
+//   projectId: "weathy-app-43294",
+//   storageBucket: "weathy-app-43294.appspot.com",
+//   messagingSenderId: "321272883381",
+//   appId: "1:321272883381:web:295e3140e4937396dab6fc",
+//   measurementId: "G-RJVEG4GZGW",
+// };
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBN0Z--JprpB8jv0JVtpioU1ybwkJkfuAE",
+  authDomain: "bookly-90b8a.firebaseapp.com",
+  projectId: "bookly-90b8a",
+  storageBucket: "bookly-90b8a.appspot.com",
+  messagingSenderId: "703161185343",
+  appId: "1:703161185343:web:7f4d3ada2bde0ea805f48a",
+};
---
+++ src/utils/router.js
@@ -0,0 +1,54 @@
+import { createBrowserRouter } from "react-router-dom";
+import { HomePage } from "../pages/HomePage";
+import { BookPage } from "../pages/BookPage";
+import { PageContainer } from "../components/PageContainer";
+import { RegisterPage } from "../pages/RegisterPage";
+import { LoginPage } from "../pages/LoginPage";
+import { NotFoundPage } from "../pages/NotFoundPage";
+
+export const getBookPageRoute = (bookId) => {
+  return `books/${bookId}`;
+};
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <PageContainer>
+        <HomePage />
+      </PageContainer>
+    ),
+  },
+  {
+    path: "books/:bookId",
+    element: (
+      <PageContainer>
+        <BookPage />
+      </PageContainer>
+    ),
+  },
+  {
+    path: "/login",
+    element: (
+      <PageContainer>
+        <LoginPage />
+      </PageContainer>
+    ),
+  },
+  {
+    path: "/register",
+    element: (
+      <PageContainer>
+        <RegisterPage />
+      </PageContainer>
+    ),
+  },
+  {
+    path: "*",
+    element: (
+      <PageContainer>
+        <NotFoundPage />
+      </PageContainer>
+    ),
+  },
+]);
